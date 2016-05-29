900d1b7d2da55eed6581e719cd510dc355e43e2a
Gaurav <flexi.aries@gmail.com>
2016-05-29 14:22:40 +0530

    added categories

--- submit_review/php/populateCategories.php
+++ submit_review/php/populateCategories.php
@@ -1,29 +1,60 @@
 <?php
-
-$servername = '127.0.0.1';
-$username = 'root';
-$password = '';
-
-$conn = new mysqli($servername, $username, $password);
-
-if ($conn->connect_error) {
-	die("Connection failed: " . $conn->connect_error);
-} 
-
-mysqli_select_db($conn, 'food_db');
-
-$query = "SELECT DISTINCT category FROM foodtable";
-$queryResult = mysqli_query($conn, $query);
-
-$select= '<select name="select">';
-$select.= '<option value="">Select Category</option>'; 
-while ($row = mysqli_fetch_array($queryResult)) {
-    $select.= '<option value="">'.$row[0].'</option>'; 
-}
-$select.='</select>';
-echo $select;
-
-mysqli_close($conn);
+echo '<select name="select">';
+echo '<option value="">Select Category</option>'; 
+echo '<option value="">Aloo Paratha</option>';
+echo '<option value="">Beverages</option>';
+echo '<option value="">Biryani</option>';
+echo '<option value="">Bruschetta</option>';
+echo '<option value="">Burger</option>';
+echo '<option value="">Butter Chicken</option>';
+echo '<option value="">Chicken Curry</option>';
+echo '<option value="">Chicken gravy</option>';
+echo '<option value="">Chicken Masala</option>';
+echo '<option value="">Chicken Paratha</option>';
+echo '<option value="">Chicken Tikka</option>';
+echo '<option value="">Chole</option>';
+echo '<option value="">Croissant</option>';
+echo '<option value="">Crostini</option>';
+echo '<option value="">Dal Makhani</option>';
+echo '<option value="">Desserts</option>';
+echo '<option value="">Egg Paratha</option>';
+echo '<option value="">Fish Curry</option>';
+echo '<option value="">Gobhi Paratha</option>';
+echo '<option value="">Honey Chilli Potato</option>';
+echo '<option value="">Maggi</option>';
+echo '<option value="">Matar Paneer</option>';
+echo '<option value="">Mix Paratha</option>';
+echo '<option value="">Momos</option>';
+echo '<option value="">Mushroom Tikka</option>';
+echo '<option value="">Mutton Curry</option>';
+echo '<option value="">Mutton Gravy</option>';
+echo '<option value="">Mutton Maggi</option>';
+echo '<option value="">Mutton Masala</option>';
+echo '<option value="">Mutton Paratha</option>';
+echo '<option value="">Mutton Tikka</option>';
+echo '<option value="">Nachos</option>';
+echo '<option value="">Noodles</option>';
+echo '<option value="">Omlette</option>';
+echo '<option value="">Paneer Butter Masala</option>';
+echo '<option value="">Paneer Paratha</option>';
+echo '<option value="">Paneer Tikka</option>';
+echo '<option value="">Paneer Tikka Masala</option>';
+echo '<option value="">Panini</option>';
+echo '<option value="">Pao Bhaji</option>';
+echo '<option value="">Pasta</option>';
+echo '<option value="">Pizza</option>';
+echo '<option value="">Prawn Curry</option>';
+echo '<option value="">Rajma</option>';
+echo '<option value="">Ravioli</option>';
+echo '<option value="">risotto</option>';
+echo '<option value="">Roll</option>';
+echo '<option value="">Salad</option>';
+echo '<option value="">Sandwich</option>';
+echo '<option value="">Shahi Paneer</option>';
+echo '<option value="">Sizzler</option>';
+echo '<option value="">Soup</option>';
+echo '<option value="">Starters</option>';
+echo '<option value="">Yellow Dal</option>';
+echo '</select>';
 
 ?>
-
